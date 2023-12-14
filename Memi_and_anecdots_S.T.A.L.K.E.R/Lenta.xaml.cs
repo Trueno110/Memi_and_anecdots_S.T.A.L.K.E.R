3fa5711d901302d41b158ff06dada77e3659235a
Trueno110 <Trueno110@yandex.ru>
2023-12-14 14:20:48 +0300

    Добавьте файлы проекта.

---
+++ Memi_and_anecdots_S.T.A.L.K.E.R/Lenta.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Memi_and_anecdots_S.T.A.L.K.E.R
+{
+    /// <summary>
+    /// Логика взаимодействия для Lenta.xaml
+    /// </summary>
+    public partial class Lenta : Window
+    {
+        public Lenta()
+        {
+            InitializeComponent();
+        }
+
+        private void AAAAA_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Vh_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Memi_and_anecdots_S.T.A.L.K.E.R/Registration.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Memi_and_anecdots_S.T.A.L.K.E.R
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Vh_Click(object sender, RoutedEventArgs e)
+        {
+            Lenta fal = new Lenta();
+            fal.Show();
+            this.Close();
+        }
+    }
+}
